c76514cca4a520c5719222db6aaa6889b6dbe92e
nraun <45707009+nraun@users.noreply.github.com>
2021-06-28 11:19:39 -0300

    Update ccBoxplots.R

--- R/ccBoxplots.R
+++ R/ccBoxplots.R
@@ -7,7 +7,7 @@ stat <- dat %>% group_by(genotype) %>%
   rstatix::wilcox_test(CI ~ condition, paired=F) %>%
   rstatix::add_xy_position(x="condition")
 
-ggplot2::ggplot(dat,aes(y=CI,x=condition)) + ggplot2::geom_boxplot() + ggplot2::facet_grid(cols=vars(genotype)) +
+ggplot2::ggplot(dat,ggplot2::aes(y=CI,x=condition)) + ggplot2::geom_boxplot() + ggplot2::facet_grid(cols=vars(genotype)) +
   ggpubr::stat_pvalue_manual(stat, tip.length = 0) + ggplot2::stat_summary(fun.data = give.n,geom="text") +
   ggplot2::scale_y_continuous(expand = c(0,0.1)) + ggplot2::labs(x=NULL)
 }
