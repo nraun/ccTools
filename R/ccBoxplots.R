d93085660f6335680c89e0ab139635151fc20755
nraun <45707009+nraun@users.noreply.github.com>
2021-06-28 10:37:58 -0300

    Create ccBoxplots.R

---
+++ R/ccBoxplots.R
@@ -0,0 +1,13 @@
+give.n <- function(x){
+  return(c(y = -0.05, label = length(x))) 
+}
+
+ccBoxplots <- function(dat){
+stat <- datfile %>% group_by(genotype) %>% 
+  rstatix::wilcox_test(CI ~ condition, paired=F) %>%
+  rstatix::add_xy_position(x="condition")
+
+ggplot2::ggplot(dat,aes(y=CI,x=condition)) + ggplot2::geom_boxplot() + ggplot2::facet_grid(cols=vars(genotype)) +
+  ggpubr::stat_pvalue_manual(stat, tip.length = 0) + ggplot2::stat_summary(fun.data = give.n,geom="text") +
+  ggplot2::scale_y_continuous(expand = c(0,0.1)) + ggplot2::labs(x=NULL)
+}
